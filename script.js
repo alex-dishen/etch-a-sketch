f65e7814677d476ae24c141ade73a151324498e7
Alex <didyshen.oleksandr@gmail.com>
2022-07-23 08:37:38 +0300

    Add defaluts

--- script.js
+++ script.js
@@ -5,16 +5,36 @@ const eraserBtn = document.querySelector('.eraser-btn');
 const sizeValue = document.querySelector('.size-value');
 const sizeSlider = document.querySelector('.size-slider');
 
+function setDefaultValues() {
+    board.style.gridTemplateColumns = `repeat(16, 1fr)`
+    board.style.gridTemplateRows = `repeat(16, 1fr)`
+
+    for (let i = 0; i < 256; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square')
+        board.appendChild(square);
+        square.addEventListener('mouseover', () => {
+            square.classList.add('color')
+        })
+
+      }
+}
+
+setDefaultValues()
+
 function setupBoard(size) {
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`
   
     for (let i = 0; i < (size * size); i++) {
       const square = document.createElement('div');
-     square.classList.add('square')
+      square.classList.add('square')
       board.appendChild(square);
+      square.addEventListener('mouseover', () => {
+        square.classList.add('color')
+      })
     }
-  }
+}
 
 function colorButton(coloredButton, firstToRemove, secondToRemove) {
     coloredButton.classList.add('colored-button');
@@ -37,3 +57,7 @@ eraserBtn.addEventListener('click', () => {
 sizeSlider.addEventListener('input', () => {
     sizeValue.textContent = `${sizeSlider.value} x ${sizeSlider.value}`
 });
+
+sizeSlider.addEventListener('mouseup', () => {
+    setupBoard(sizeSlider.value);
+})
