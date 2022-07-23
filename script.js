db22246d0dbd0604a09b7a90f40088e6e843ed46
Alex <didyshen.oleksandr@gmail.com>
2022-07-23 07:31:40 +0300

    Create a function to manipulate board

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
+const board = document.querySelector('.board');
 const colorBtn = document.querySelector('.color-btn');
 const rainbowBtn = document.querySelector('.rainbow-btn');
 const eraserBtn = document.querySelector('.eraser-btn');
 const sizeValue = document.querySelector('.size-value');
 const sizeSlider = document.querySelector('.size-slider');
+
+function setupBoard(size) {
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`
+  
+    for (let i = 0; i < (size * size); i++) {
+      const square = document.createElement('div');
+     square.classList.add('square')
+      board.appendChild(square);
+    }
+  }
 
 function colorButton(coloredButton, firstToRemove, secondToRemove) {
     coloredButton.classList.add('colored-button');
