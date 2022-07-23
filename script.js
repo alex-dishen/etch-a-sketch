65575cc8098eff8fc8f573b0be21b69aba5e3860
Alex <didyshen.oleksandr@gmail.com>
2022-07-23 12:43:01 +0300

    Change behaviour of mouse over

--- script.js
+++ script.js
@@ -1,26 +1,12 @@
+let currentColor = '#333333';
+let currentMode = 'color';
+
 const board = document.querySelector('.board');
 const colorBtn = document.querySelector('.color-btn');
 const rainbowBtn = document.querySelector('.rainbow-btn');
 const eraserBtn = document.querySelector('.eraser-btn');
 const sizeValue = document.querySelector('.size-value');
 const sizeSlider = document.querySelector('.size-slider');
-
-function setDefaultValues() {
-    board.style.gridTemplateColumns = `repeat(16, 1fr)`
-    board.style.gridTemplateRows = `repeat(16, 1fr)`
-
-    for (let i = 0; i < 256; i++) {
-        const square = document.createElement('div');
-        square.classList.add('square')
-        board.appendChild(square);
-        square.addEventListener('mouseover', () => {
-            square.classList.add('color')
-        })
-
-      }
-}
-
-setDefaultValues()
 
 function setupBoard(size) {
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`
@@ -30,11 +16,30 @@ function setupBoard(size) {
       const square = document.createElement('div');
       square.classList.add('square')
       board.appendChild(square);
-      square.addEventListener('mouseover', () => {
-        square.classList.add('color')
-      })
+      square.addEventListener('mouseover', changeColor);
+      square.addEventListener('mousedown', changeColor);
     }
 }
+
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
+
+function changeColor(e) {
+    if (e.type === 'mouseover' && !mouseDown) return
+    console.log(e)
+    if (currentMode === 'rainbow') {
+      const randomR = Math.floor(Math.random() * 256)
+      const randomG = Math.floor(Math.random() * 256)
+      const randomB = Math.floor(Math.random() * 256)
+      e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
+    } else if (currentMode === 'color') {
+      e.target.style.backgroundColor = currentColor
+    } else if (currentMode === 'eraser') {
+      e.target.style.backgroundColor = '#fefefe'
+    }
+  }
 
 function colorButton(coloredButton, firstToRemove, secondToRemove) {
     coloredButton.classList.add('colored-button');
@@ -61,3 +66,8 @@ sizeSlider.addEventListener('input', () => {
 sizeSlider.addEventListener('mouseup', () => {
     setupBoard(sizeSlider.value);
 })
+
+window.onload = () => {
+    setupBoard(16);
+    colorBtn.classList.add('colored-button');
+}
